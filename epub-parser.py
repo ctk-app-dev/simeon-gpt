f7d94940340b11c416b7b1e36c537fd10fea7586
ManchurianMan <alasbenfoiledagain@gmail.com>
2024-02-24 00:03:16 -0500

    path disambiguation

--- epub-parser.py
+++ epub-parser.py
@@ -1,8 +1,10 @@
 import json
 from bs4 import BeautifulSoup
 from collections import defaultdict
+from pathlib import Path
 
-base_path = "c:/simeon-gpt/assets/OEBPS/Text"
+
+base_path = Path(__file__).parent / "assets" / "OEBPS" / "Text" # this will make sure it works regardless of OS/cwd
 
 html_doc = open(f"{base_path}/Trinity.xhtml", "r")
 
