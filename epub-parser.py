d20f8f3b9700636759849d6543e6f012548c20b7
ManchurianMan <alasbenfoiledagain@gmail.com>
2024-03-01 14:44:55 -0500

    begin function definition

--- epub-parser.py
+++ epub-parser.py
@@ -9,15 +9,20 @@ base_path = Path(__file__).parent / "assets" / "OEBPS" / "Text" # this will make
 html_doc = open(f"{base_path}/Trinity.xhtml", "r")
 
 # Assuming 'html_doc' is your xhtml document
-soup = BeautifulSoup(html_doc, 'lxml')
+soup = BeautifulSoup(html_doc, "lxml")
 
 texts = defaultdict(list)
 
 # Finding all the h4 elements
 h4 = soup.find_all('h4')
 for h in h4:  # Iterating over elements to determine the trinity
-    q = h.text.replace('\xa0', ' ').title()  # Normalizing and formatting the text
-    for sibling in h.next_siblings:  # Iterating over sibling elements
+    q = h.text.replace('\xa0', ' ').title()
+    # Pseudocode:
+    # for each heading, get all the sibling 'p' elements until you hit another heading.
+    # that should be its own function.
+
+    # then, once you have the xhtml for each week, you can perform other logic; another function
+    for sibling in h.next_siblings:
         # Skip processing if the sibling is not a paragraph ('p')
         if sibling.name != "p":
             continue
@@ -26,7 +31,20 @@ for h in h4:  # Iterating over elements to determine the trinity
 
         # Append the text to the dictionary only if it contains any of the weekdays
         if any(day in text for day in weekdays):
-            texts[q].append(text)
+            texts[q].append(text.split('Ps.')[0].strip().replace('\n', ' '))
 # Converting to a regular dict for readability
 print(json.dumps(texts, indent=2))
 
+
+
+def get_each_week_xhtml(xhtml_path):
+    base_path = Path(__file__).parent / "assets" / "OEBPS" / "Text" # code works regardless of OS/cwd
+    html_doc = open(f"{base_path}/{xhtml_path}", "r")
+    soup = BeautifulSoup(html_doc, "lxml")
+
+    # Finding all the h4 elements
+    h4 = soup.find_all('h4')
+    for h in h4:  # Iterating over elements to determine the trinity
+        q = h.text.replace('\xa0', ' ').title()
+        
+    return soup
